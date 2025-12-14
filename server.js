20bd1a3d4712f155a31d2edf32361291f4766a16
alejandro012004 <150953789+alejandro012004@users.noreply.github.com>
2025-12-14 21:56:02 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+const buildPath = path.join(__dirname, 'build', 'web');
+
+app.use(express.static(buildPath));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor de Flutter Web corriendo en el puerto ${port}`);
+});
